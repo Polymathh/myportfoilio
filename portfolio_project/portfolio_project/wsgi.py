1d490bee769d3124e436b26eb7f8e35d07d34b8f
Polymathh <wambugum110@gmail.com>
2025-10-06 17:14:44 +0300

    initial commit

--- portfolio_project/portfolio_project/wsgi.py
+++ portfolio_project/portfolio_project/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import shutil
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TMP_DB = '/tmp/db.sqlite3'
+LOCAL_DB = os.path.join(BASE_DIR, 'db.sqlite3')  # your local database
+
+# Copy the database if it doesn't exist in /tmp
+if not os.path.exists(TMP_DB) and os.path.exists(LOCAL_DB):
+    shutil.copy(LOCAL_DB, TMP_DB)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+
 
