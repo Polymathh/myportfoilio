ce84692a08b211a60cd83590f33b71ab292fc426
Polymathh <wambugum110@gmail.com>
2025-10-06 16:08:13 +0300

    initial commit

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -141,7 +141,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = "wambuguworking@gmail.com"
+EMAIL_HOST_PASSWORD = "lumf lagb okyo bdto"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
