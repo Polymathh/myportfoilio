5a98efc11365703c669d4934ab5bf149f7b5a5bd
Polymathh <wambugum110@gmail.com>
2025-10-06 17:48:35 +0300

    initial commit

--- portfolio_project/portfolio_project/asgi.py
+++ portfolio_project/portfolio_project/asgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
+from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
-from django.core.asgi import get_asgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio_project.settings")
+django.setup()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
+# Run migrations on Vercel
+if os.environ.get("VERCEL") == "1":
+    try:
+        call_command("migrate", interactive=False)
+    except Exception as e:
+        print("Migration failed:", e)
 
-application = get_asgi_application()
+application = get_wsgi_application()
+
+app = application
