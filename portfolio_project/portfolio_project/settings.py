c1bc131c6e30604ae2dd10cfb55a0c5252b59a55
Polymathh <wambugum110@gmail.com>
2025-10-08 12:26:46 +0300

    db sqlite3

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -75,12 +75,19 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-ON_VERCEL = os.environ.get("VERCEL") == "1"
+# ON_VERCEL = os.environ.get("VERCEL") == "1"
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": str(BASE_DIR / "db.sqlite3") if not ON_VERCEL else "/tmp/db.sqlite3",
+#     }
+# }
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": str(BASE_DIR / "db.sqlite3") if not ON_VERCEL else "/tmp/db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
